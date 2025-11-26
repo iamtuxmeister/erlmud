db52eaf9b62013e065a4d3a152ddb97933e0bffa
root <root@localhost.localdomain>
2025-11-26 21:52:31 +0000

    first commit

---
+++ erlmud/README.md
@@ -0,0 +1,9 @@
+erlmud
+=====
+
+An OTP application
+
+Build
+-----
+
+    $ rebar3 compile
---
+++ erlmud/src/erlmud_app.erl
@@ -0,0 +1,18 @@
+%%%-------------------------------------------------------------------
+%% @doc erlmud public API
+%% @end
+%%%-------------------------------------------------------------------
+
+-module(erlmud_app).
+
+-behaviour(application).
+
+-export([start/2, stop/1]).
+
+start(_StartType, _StartArgs) ->
+    erlmud_sup:start_link().
+
+stop(_State) ->
+    ok.
+
+%% internal functions
